d03004324ae9f9e8352c57435aebd5cb2191da4b
EmanueleBrusa <emanuelebrusa@hotmail.it>
2023-05-22 07:23:13 +0200

    completamento contenuto pre-stilizzazione

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
-let pulsante = document.getElementById('pulsante');
+const pulsanteGenera = document.getElementById('genera');
 
-
-pulsante.addEventListener('click', function() { 
+pulsanteGenera.addEventListener('click', function() { 
     //input km e anni utente
     let km_utente = document.getElementById("km").value;
     let anni_utente = document.getElementById("eta").value;
@@ -11,28 +10,28 @@ pulsante.addEventListener('click', function() {
 
     //CALCOLO PREZZO INTERO DEL BIGLIETTO
     let prezzo_intero = (km_utente * prezzo_al_chilometro);
+   
 
-    //DICHIARIAZIONE VARIABILE PER LO SCONTO
+    //DICHIARIAZIONE VARIABILE PER LO SCONTO e NOME OFFERTA BIGLIETTO
     let scontoDaApplicare;
+    let nomeBiglietto;
 
     //CONDIZIONE
     //SCONTO MINORENNI
-    if (anni_utente < 18) {
-
+    if (anni_utente === "2" ) {
+        nomeBiglietto = "Biglietto minori";       
         scontoDaApplicare = 0.20;
         console.log("sconto del 20%: " + scontoDaApplicare);
-
     }
     //SCONTO OVER 65
-    else if (anni_utente >= 65) {
-
+    else if (anni_utente === "3") {
+        nomeBiglietto = "Biglietto over 65";
         scontoDaApplicare = 0.40;
         console.log("sconto del 40%: " + scontoDaApplicare);
-
     }
     //SCONTO A 0
-    else {//if(anni_utente >=18 || anni_utente <= 65) {
-
+    else {
+        nomeBiglietto = "Biglietto standard"
         scontoDaApplicare = 0;
         console.log("sconto 0%: " + scontoDaApplicare);
     }
@@ -45,6 +44,30 @@ pulsante.addEventListener('click', function() {
     console.log("prezzo finale: " + prezzo_finale_fixed);
 
     //RISULTATO A VIDEO
-    let input = document.getElementById("risultato").innerHTML = prezzo_finale_fixed;
+    document.getElementById("offerta").innerHTML = nomeBiglietto;
+    document.getElementById("risultato").innerHTML = prezzo_finale_fixed + " €";
+    document.getElementById("nomeCognomeDef").innerHTML = document.getElementById("nomecognome").value;
+
+    //Carrozza e Codice CP
+    let carrozzaRandom = Math.floor(Math.random() * 11);
+    document.getElementById("numeroCarrozza").innerHTML =  carrozzaRandom;
+
+    let codiceCpRandom = Math.floor(Math.random() * 100001);
+    document.getElementById("codiceCp").innerHTML =  codiceCpRandom;
 
 });
+
+//PULSANTE AZZERA
+const pulsanteAzzera = document.getElementById("azzera");
+pulsanteAzzera.addEventListener('click', function() {
+    document.getElementById("nomecognome").value = "";
+    document.getElementById("km").value = "";
+
+    document.getElementById("offerta").innerHTML = "";
+    document.getElementById("numeroCarrozza").innerHTML = "";
+    document.getElementById("codiceCp").innerHTML = "";
+    document.getElementById("risultato").innerHTML = "00.0 €" ;
+
+})
+
+document.getElementById("risultato").innerHTML = "00.0 €" ;
